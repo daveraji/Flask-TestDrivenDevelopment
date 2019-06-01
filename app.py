ce02743558e4a95fdccf1ef04004a1a0cbd8cedf
David Raji <daveraji@hotmai.com>
2019-06-01 15:38:13 -0700

    initial project version

---
+++ app.py
@@ -0,0 +1,17 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
+
+#configuration
+DATABASE = 'flaskr.db'
+DEBUG = 'True'
+SECRET_KEY = 'tasty'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+#create and initialize app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+if __name__ == '__main__':
+    app.run()
