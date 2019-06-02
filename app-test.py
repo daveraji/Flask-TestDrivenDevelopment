4bab23d27f0fba7676378c75df181ca779d32343
David Raji <daveraji@hotmai.com>
2019-06-01 23:52:52 -0700

    finished building login page and database test structure and started working on the features and functions themselves in js, css and python

--- app-test.py
+++ app-test.py
@@ -1,17 +1,64 @@
 import unittest
 import os
-from app import app
+import tempfile
+import app
 
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
-        tester = app.test_client(self)
+        tester = app.app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
     def test_database(self):
         tester = os.path.exists('flaskr.db')
-        self.assertTrue(tester)
+        self.assertEqual(tester, True)
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        #set up a blank temporary database before each test
+        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+        app.init_db()
+
+    def tearDown(self):
+        #destroys the blank database are every test
+        os.close(self.db_fd)
+        os.unlink(app.app.config['DATABASE'])
+
+    def login(self, username, password):
+        #this is the login helper function
+        return self.app.post('/login', data=dict(username=username, password=password), follow_redirects=True)
+
+    def logout(self):
+        #logout helper function
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_empty_db(self):
+        #makes sure that the database is blank
+        rv = self.app.get('/')
+        assert b'No entries here so far' in rv.data
+
+    def test_login_logout(self):
+        #test login and logout using helper funtions that you outlined earlier
+        rv = self.login(app.app.config['USERNAME'], app.app.config['PASSWORD'])
+        assert b'You were logged in' in rv.data
+        rv = self.logout()
+        assert b'You were logged out' in rv.data
+        rv = self.login(app.app.config['USERNAME'] + 'x', app.app.config['PASSWORD'])
+        assert b'Invalid username' in rv.data
+        rv = self.login(app.app.config['USERNAME'], app.app.config['PASSWORD'] + 'x')
+        assert b'Invalid password' in rv.data
+
+    def test_messages(self):
+        #make sure that a user can post test_messages
+        self.login(app.app.config['USERNAME'], app.app.config['PASSWORD'])
+        rv = self.app.post('/add', data=dict(title='<Hello>', text='<strong>HTML</strong> allowed here'), follow_redirects=True)
+        assert b'No entries here so far' not in rv.data
+        assert b'$lt;Hello&gt;' in rv.data
+        assert b'<strong>HTML</strong> allowed here' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
--- app.py
+++ app.py
@@ -12,6 +12,41 @@ PASSWORD = 'admin'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+#connect to the DATABASE
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+# create the database
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+#open the database connection
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+#close database connection
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
+def show_entries():
+    db=get_db()
+    cur = db.execute('select * from entries order by id description')
+    entries = cur.fetchall()
+    return render_template('index.html', entries=entries)
+
+
 
 if __name__ == '__main__':
+    init_db()
     app.run()
